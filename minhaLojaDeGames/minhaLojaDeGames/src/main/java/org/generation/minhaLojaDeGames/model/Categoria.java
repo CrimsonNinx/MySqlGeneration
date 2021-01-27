d7d0dab59dd1b52c9880b17a7481ebeed8c7ddc6
Erick Costa Ferreira <74927000+CrimsonNinx@users.noreply.github.com>
2021-01-27 15:15:02 -0300

    LojaGames

---
+++ minhaLojaDeGames/minhaLojaDeGames/src/main/java/org/generation/minhaLojaDeGames/model/Categoria.java
@@ -0,0 +1,36 @@
+package org.generation.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	private String descricaoCategoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
+}
