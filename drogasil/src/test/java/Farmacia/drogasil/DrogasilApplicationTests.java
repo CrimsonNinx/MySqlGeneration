e5291787db1b7dcb95fba9f1c03e6954385a552b
Erick Costa Ferreira <74927000+CrimsonNinx@users.noreply.github.com>
2021-01-26 21:15:36 -0300

    Api para farmacia

---
+++ drogasil/src/test/java/Farmacia/drogasil/DrogasilApplicationTests.java
@@ -0,0 +1,13 @@
+package Farmacia.drogasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
