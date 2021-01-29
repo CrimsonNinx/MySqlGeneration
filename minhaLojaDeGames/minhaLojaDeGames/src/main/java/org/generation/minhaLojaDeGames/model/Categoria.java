42476fa92872911ec8cd7a3b5e7cbac7bb03409b
Erick Costa Ferreira <74927000+CrimsonNinx@users.noreply.github.com>
2021-01-28 22:09:08 -0300

    minhalojagames

--- minhaLojaDeGames/minhaLojaDeGames/src/main/java/org/generation/minhaLojaDeGames/model/Categoria.java
+++ minhaLojaDeGames/minhaLojaDeGames/src/main/java/org/generation/minhaLojaDeGames/model/Categoria.java
@@ -1,11 +1,17 @@
 package org.generation.minhaLojaDeGames.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -17,6 +23,10 @@ public class Categoria {
 
 	@NotNull
 	private String descricaoCategoria;
+	
+	@OneToMany(mappedBy = "categoria", cascade =CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public long getId() {
 		return id;
@@ -33,4 +43,13 @@ public class Categoria {
 	public void setDescricaoCategoria(String descricaoCategoria) {
 		this.descricaoCategoria = descricaoCategoria;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
 }
